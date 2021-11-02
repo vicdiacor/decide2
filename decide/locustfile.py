9ec0bd4f910548e14922936d4e31d5a30685d4fa
serarirud <sergiariasruda@gmail.com>
2021-11-02 11:58:57 +0100

    Deleted locustfile.py

--- decide/locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task
-
-class locust_test(HttpUser):
-    @task
-    def load_test(self):
-        self.client.get('/admin')
-        self.client.get('/mixnet')
-        self.client.get('/voting')

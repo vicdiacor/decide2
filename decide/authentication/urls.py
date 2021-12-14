a1567bf97c2ba4c53b49a786025ec185a77d22c9
antoniofm00 <antfunmej@alum.us.es>
2021-12-14 20:16:34 +0100

    SOLUCIONAD ERROR SIGNIN
    Solucionado error en signin que devolvía el formulario sin errores sin loguear

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from authentication.views import registro, cerrar_sesion, iniciar_sesion
+from authentication.views import registro, cerrar_sesion, iniciar_sesion, GetUserView, LogoutView, RegisterView
 
 urlpatterns = [
 
     path('register/', registro),
     path('cerrar_sesion/', cerrar_sesion),
-    path('iniciar_sesion/', iniciar_sesion)
+    path('iniciar_sesion/', iniciar_sesion),
+    path('login/', obtain_auth_token),
+    path('logout/', LogoutView.as_view()),
+    path('getuser/', GetUserView.as_view()),
+    path('register/', RegisterView.as_view()),
 ]
--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, redirect
 from decide import settings
 
+from rest_framework.response import Response
+from rest_framework.status import (
+        HTTP_201_CREATED,
+        HTTP_400_BAD_REQUEST,
+        HTTP_401_UNAUTHORIZED
+)
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+
+from .serializers import UserSerializer
 from django.contrib.auth.forms import AuthenticationForm
 
 from authentication.forms import SignUpForm
@@ -30,55 +44,57 @@ def cerrar_sesion(request):
 
 def iniciar_sesion(request):
     if request.method == "POST":
-        form = AuthenticationForm(request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username=form.cleaned_data.get('username')
-            password=form.cleaned_data.get('password1')
-            user=authenticate(request, username,password)
+            password=form.cleaned_data.get('password')
+            user=authenticate(request, username=username,password=password)
+            
             if user is not None:
                 login(request, user)
+                
                 return redirect(inicio)
     
     else:
         form=AuthenticationForm()
     return render(request, 'signin.html', {'form': form, 'STATIC_URL':settings.STATIC_URL})    
 
-# class GetUserView(APIView):
-#     def post(self, request):
-#         key = request.data.get('token', '')
-#         tk = get_object_or_404(Token, key=key)
-#         return Response(UserSerializer(tk.user, many=False).data)
+class GetUserView(APIView):
+    def post(self, request):
+        key = request.data.get('token', '')
+        tk = get_object_or_404(Token, key=key)
+        return Response(UserSerializer(tk.user, many=False).data)
 
 
-# class LogoutView(APIView):
-#     def post(self, request):
-#         key = request.data.get('token', '')
-#         try:
-#             tk = Token.objects.get(key=key)
-#             tk.delete()
-#         except ObjectDoesNotExist:
-#             pass
+class LogoutView(APIView):
+    def post(self, request):
+        key = request.data.get('token', '')
+        try:
+            tk = Token.objects.get(key=key)
+            tk.delete()
+        except ObjectDoesNotExist:
+            pass
 
-#         return Response({})
+        return Response({})
 
 
-# class RegisterView(APIView):
-#     def post(self, request):
-#         key = request.data.get('token', '')
-#         tk = get_object_or_404(Token, key=key)
-#         if not tk.user.is_superuser:
-#             return Response({}, status=HTTP_401_UNAUTHORIZED)
+class RegisterView(APIView):
+    def post(self, request):
+        key = request.data.get('token', '')
+        tk = get_object_or_404(Token, key=key)
+        if not tk.user.is_superuser:
+            return Response({}, status=HTTP_401_UNAUTHORIZED)
 
-#         username = request.data.get('username', '')
-#         pwd = request.data.get('password', '')
-#         if not username or not pwd:
-#             return Response({}, status=HTTP_400_BAD_REQUEST)
+        username = request.data.get('username', '')
+        pwd = request.data.get('password', '')
+        if not username or not pwd:
+            return Response({}, status=HTTP_400_BAD_REQUEST)
 
-#         try:
-#             user = User(username=username)
-#             user.set_password(pwd)
-#             user.save()
-#             token, _ = Token.objects.get_or_create(user=user)
-#         except IntegrityError:
-#             return Response({}, status=HTTP_400_BAD_REQUEST)
-#         return Response({'user_pk': user.pk, 'token': token.key}, HTTP_201_CREATED)
+        try:
+            user = User(username=username)
+            user.set_password(pwd)
+            user.save()
+            token, _ = Token.objects.get_or_create(user=user)
+        except IntegrityError:
+            return Response({}, status=HTTP_400_BAD_REQUEST)
+        return Response({'user_pk': user.pk, 'token': token.key}, HTTP_201_CREATED)

5f9a9417c142f346843d8fde4e4c7d67e6d522dc
felconmar <felconmar@alum.us.es>
2021-12-17 19:24:59 +0100

    "#4":Añade parte de la funcionalidad
    Funcionalidad que permite al administrador aceptar y rechazar solicitudes

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Census, ParentGroup, Request, RequestStatus
 
-from .models import Census, ParentGroup, Request
 
 
 class CensusAdmin(admin.ModelAdmin):
@@ -13,12 +14,30 @@ class ParentGroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'isPublic')
     list_filter = ('name', 'isPublic')
 
+def accept(modeladmin, request, queryset):
+    for v in queryset.all():
+        voter = User.objects.get(pk=v.voter_id)
+        group = ParentGroup.objects.get(pk=v.group_id)
+        #Guardar el group en el voter y guardarlo
+        v.status = RequestStatus.ACCEPTED        
+        v.save()
+
+def reject(modeladmin, request, queryset):
+    for v in queryset.all():
+        v.status = RequestStatus.REJECTED        
+        v.save()
+
 class RequestAdmin(admin.ModelAdmin):
     list_display = ('voter_id', 'group_id')
     list_filter = ('voter_id', 'group_id')
 
     search_fields = ('voter_id', 'group_id')
 
+           
+                    
+
 admin.site.register(Census, CensusAdmin)
 admin.site.register(ParentGroup, ParentGroupAdmin)
 admin.site.register(Request, RequestAdmin)
+
+

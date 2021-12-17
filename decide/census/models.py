d15e6a302aecaaef97a0f1877b92b86bab44f736
felconmar <felconmar@alum.us.es>
2021-12-17 18:18:47 +0100

    "#5":Añade status al modelo de Request
    Permite aceptar, rechazar o que se generen por defecto requests en pending

--- decide/census/models.py
+++ decide/census/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import Group
+from enum import Enum
 
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
@@ -12,8 +13,14 @@ class Census(models.Model):
 class ParentGroup(Group):
     isPublic = models.BooleanField(default=False)
 
-
+class RequestStatus(Enum):
+        ACCEPTED = 'ACCEPTED' 
+        REJECTED = 'REJECTED'
+        PENDING = 'PENDING'
+        @classmethod
+        def choices(cls):
+            return tuple((i.name, i.value) for i in cls)
 class Request(models.Model):
     voter_id = models.PositiveIntegerField()
     group_id = models.PositiveIntegerField()
-    
+    status = models.CharField(max_length=255, choices=RequestStatus.choices(), default=RequestStatus.PENDING)

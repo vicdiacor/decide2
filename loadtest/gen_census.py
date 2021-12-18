919f78271234a3213f03f7ca3a8e63060dd05cf4
serarirud <sergiariasruda@gmail.com>
2021-12-18 14:27:06 +0100

    "#3": Modificado gen_census.py
    Añadido en gen_census.py una forma automática de crear una votación. Falta que la votación sea aleatoria.

--- loadtest/gen_census.py
+++ loadtest/gen_census.py
@@ -1,11 +1,17 @@
 import json
 import requests
+import os
 
 
 HOST = "http://localhost:8000"
 USER = "decide1"
 PASS = "decide1234"
-VOTING = 1
+
+def login(user=USER, password=PASS):
+    data = {'username': user, 'password': password}
+    response = requests.post(HOST + '/authentication/login/', data=data)
+    token = response.json()
+    return token
 
 
 def create_voters(filename):
@@ -16,9 +22,7 @@ def create_voters(filename):
     with open(filename) as f:
         voters = json.loads(f.read())
 
-    data = {'username': USER, 'password': PASS}
-    response = requests.post(HOST + '/authentication/login/', data=data)
-    token = response.json()
+    token = login()
 
     voters_pk = []
     invalid_voters = []
@@ -26,27 +30,78 @@ def create_voters(filename):
         token.update({'username': username, 'password': pwd})
         response = requests.post(HOST + '/authentication/register/', data=token)
         if response.status_code == 201:
-            voters_pk.append(response.json().get('user_pk'))
+            voters_pk.append({'id': response.json().get('user_pk'), 'username': username, 'pass': pwd})
         else:
             invalid_voters.append(username)
     return voters_pk, invalid_voters
 
 
-def add_census(voters_pk, voting_pk):
+def add_census(voters, voting_pk):
     """
     Add to census all voters_pk in the voting_pk.
     """
-    data = {'username': USER, 'password': PASS}
-    response = requests.post(HOST + '/authentication/login/', data=data)
-    token = response.json()
+    token = login()
 
-    data2 = {'voters': voters_pk, 'voting_id': voting_pk}
+    data2 = {'voters': [v['id'] for v in voters], 'voting_id': voting_pk}
     auth = {'Authorization': 'Token ' + token.get('token')}
     response = requests.post(HOST + '/census/', json=data2, headers=auth)
     print(response.text)
 
+def create_voting() -> int:
+    token = login()
+    data = {
+            'name': 'Votación para estadísticas',
+            'desc': 'Esta votación se crea de prueba para poder ver las estadísticas de manera más fácil',
+            'question': 'Animal preferido',
+            'question_opt': ['Gato', 'Perro', 'Hamster', 'Conejo', 'Carpincho', 'Jirafa']
+        }
+    auth = {'Authorization': 'Token ' + token.get('token')}
+    response = requests.post(HOST + '/voting/', json=data, headers=auth)
+    print(response.status_code)
+    voting_id = json.loads(response.text)['id']
 
+    data = {'action': 'start'}
+    response = requests.put(f'{HOST}/voting/{voting_id}/', json=data, headers=auth)
+    print(response.status_code)
 
-voters, invalids = create_voters('voters.json')
-add_census(voters, VOTING)
-print("Create voters with pk={0} \nInvalid usernames={1}".format(voters, invalids))
+    return voting_id
+
+def vote(filename):
+    voters, invalids = create_voters(filename)
+    if len(invalids) > 0:
+        raise Exception('Borra los usuarios existentes antes de empezar')
+
+    voting_id = create_voting()
+
+    add_census(voters, voting_id)
+    
+    print('Votaciones: ')
+    for voter in voters:
+        id_, username, pwd = voter['id'], voter['username'], voter['pass']
+        token = login(username, pwd)
+        auth = {'Authorization': 'Token ' + token.get('token')}
+        data = {
+                "voting": voting_id,
+                "voter": id_,
+                "vote": {'Gato': 0, 'Perro': 0, 'Hamster': 1, 'Conejo': 0, 'Carpincho': 0, 'Jirafa': 0}
+            }
+        response = requests.post(HOST + '/store/', json=data, headers=auth)
+        print(response.status_code)
+    
+    token = login()
+    auth = {'Authorization': 'Token ' + token.get('token')}
+    data = {'action': 'stop'}
+    response = requests.put(f'{HOST}/voting/{voting_id}/', json=data, headers=auth)
+    print(response.status_code)
+
+    data = {'action': 'tally'}
+    response = requests.put(f'{HOST}/voting/{voting_id}/', json=data, headers=auth)
+    print(response.status_code)
+    print(f'Ve a {HOST}/visualizer/{voting_id} para ver los resultados.')
+
+current_path = '/'.join(__file__.split('/')[:-1])
+vote(current_path + '/voters.json')
+# print(create_voting())
+# voters, invalids = create_voters(current_path + '/voters.json')
+# add_census(voters, create_voting())
+# print("Create voters with pk={0} \nInvalid usernames={1}".format(voters, invalids))

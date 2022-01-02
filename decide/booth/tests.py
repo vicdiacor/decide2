6e19c347407960283b352ef7b78586f31ea05c76
vicdiacor <vicdiacor@alum.us.es>
2022-01-02 18:28:48 +0100

    "#72": Test votación múltiple en booth creado
    Se ha creado un test de Selenium para las votaciones mútiples desde la interfaz de booth

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from time import sleep
 from base.tests import SeleniumBaseTestCase
 from store.models import Vote
 from voting.models import Voting, Question, QuestionOption
@@ -54,17 +53,30 @@ class SeleniumTestCase(SeleniumBaseTestCase):
         c = Census(voter_id=u1.id, voting_id=v.id, adscripcion= "awadwadd")
         c.save()
 
+        q = Question(desc='pregunta Multiple_Choice',type='MC')
+        q.save()
+        opt1 = QuestionOption(question=q, option='opcion 1',number=1)
+        opt1.save()
+        opt2 = QuestionOption(question=q, option='opcion 2',number=2)
+        opt2.save()
+
+        v = Voting(name='Voting Multiple_Choice', question=q, pk= 501, desc="Description")
+        v.save()
+        v.auths.add(a)
+        v.save()
        
+        c = Census(voter_id=u1.id, voting_id=v.id, adscripcion= "awadwadd")
+        c.save()
         
         return super().setUp()    
 
     def tearDown(self):
         super().tearDown()
     
-        
+    
     def test_booth_single_option_vote(self):
 
-        # Accedemos a la votación antes de que empiece
+        # Accedemos a la votación antes de que empiece y da error
         
         self.driver.get("{}/booth/{}".format(self.live_server_url,500))
         heading1 = self.driver.find_element_by_tag_name('h1').text
@@ -90,24 +102,28 @@ class SeleniumTestCase(SeleniumBaseTestCase):
         self.driver.find_element_by_id('username').send_keys('username1Grupo1')
         self.driver.find_element_by_id('password').send_keys('password',Keys.ENTER)
         
-        # El usuario selecciona una opción y pulsa en votar
+        # El usuario se encuentra con un formulario de tipo RADIO
 
         wait = WebDriverWait(self.driver, 10)
+        botonRadio= wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='q2']")))
+        
+        self.assertEquals(botonRadio.get_attribute("type"),"radio")
 
-        wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='q2']"))).click()
+        # El usuario selecciona una única opción y envía el voto con éxito
+
+        botonRadio.click()
         self.driver.find_element_by_class_name('btn-primary').click()
-       
         mensaje_exito= str(wait.until(EC.presence_of_element_located((By.XPATH,"//div[@role='alert']"))).text).strip()
         self.assertEquals(mensaje_exito,'× Conglatulations. Your vote has been sent')
         self.driver.find_element_by_xpath("//button[@class='close']").click()
 
-        # El usuario intenta votar otra vez
+        # El usuario intenta votar otra vez pero ya no está autorizado
         wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='q1']"))).click()
         self.driver.find_element_by_class_name('btn-primary').click()
         mensaje_no_autorizado= str(wait.until(EC.presence_of_element_located((By.XPATH,"//div[@role='alert']"))).text).strip()
         self.assertEquals(mensaje_no_autorizado,'× Error: Unauthorized')
 
-        # Comprobamos que se ha creado un único voto correctamnete
+        # Comprobamos que se ha creado un único voto correctamnete encriptado
         self.login()
         self.driver.find_element_by_link_text('Votes').click()
         votes_sent= self.driver.find_elements_by_link_text('500: 101')
@@ -121,10 +137,89 @@ class SeleniumTestCase(SeleniumBaseTestCase):
 
         self.assertEquals(voting_id,"500")
         self.assertEquals(voter_id,"101")
+        self.assertTrue(a_encrypted!="" and len(a_encrypted)==77)
+        self.assertTrue(b_encrypted!="" and len(a_encrypted)==77)
+    
+    def test_booth_multiple_option_vote(self):
+
+        # Accedemos a la votación antes de que empiece y da error
+        
+        self.driver.get("{}/booth/{}".format(self.live_server_url,501))
+        heading1 = self.driver.find_element_by_tag_name('h1').text
+        self.assertEquals(heading1,'Not Found')
+    
+        # Iniciamos las votaciones
+        self.login()
+        self.driver.find_element_by_link_text('Votings').click()
+        self.driver.find_element_by_id('action-toggle').click()
+        select = Select(self.driver.find_element_by_name('action'))
+        select.select_by_visible_text('Start')
+        self.driver.find_element_by_class_name('actions').find_element_by_class_name('button').click()
+        self.driver.find_element_by_link_text('TERMINAR SESIÓN').click()
+
+        # Intentamos acceder a la votación una vez haya comenzado
+        self.driver.get("{}/booth/{}".format(self.live_server_url,501))
+
+
+        # El usuario se logea
+        self.driver.find_element_by_id('username').send_keys('username1Grupo1')
+        self.driver.find_element_by_id('password').send_keys('password',Keys.ENTER)
+        
+        # Se muestra un formulario de tipo CHECKBOX
+        wait = WebDriverWait(self.driver, 10)
+        
+        checkbox_1= wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='q1']")))
+        checkbox_2= wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='q2']")))
+        self.assertEquals(checkbox_1.get_attribute("type"),"checkbox")
+        self.assertEquals(checkbox_2.get_attribute("type"),"checkbox")
+
+        # El usuario selecciona DOS opciones simultáneas y vota con éxito
+
+        checkbox_1.click()
+        checkbox_2.click()
+        self.driver.find_element_by_class_name('btn-primary').click()
+        
+        mensaje_exito= str(wait.until(EC.presence_of_element_located((By.XPATH,"//div[@role='alert']"))).text).strip()
+        self.assertEquals(mensaje_exito,'× Conglatulations. Your vote has been sent')
+        self.driver.find_element_by_xpath("//button[@class='close']").click()
+
+        # El usuario intenta votar otra vez pero ya no está autorizado
+        wait.until(EC.element_to_be_clickable((By.XPATH,"//input[@id='q1']"))).click()
+        self.driver.find_element_by_class_name('btn-primary').click()
+        mensaje_no_autorizado= str(wait.until(EC.presence_of_element_located((By.XPATH,"//div[@role='alert']"))).text).strip()
+        self.assertEquals(mensaje_no_autorizado,'× Error: Unauthorized')
+
+        # Comprobamos que se han creado DOS VOTOS correctamente
+        self.login()
+        self.driver.find_element_by_link_text('Votes').click()
+        votes_sent= self.driver.find_elements_by_link_text('501: 101')
+        self.assertEquals(len(votes_sent), 2)
+        
+        # Primer voto correctamente guardado y encriptado
+        votes_sent[0].click()
+        voting_id= self.driver.find_element_by_xpath("//input[@id='id_voting_id']").get_attribute('value')
+        voter_id= self.driver.find_element_by_xpath("//input[@id='id_voter_id']").get_attribute('value')
+        a_encrypted= self.driver.find_element_by_xpath("//textarea[@id='id_a']").text
+        b_encrypted= self.driver.find_element_by_xpath("//textarea[@id='id_b']").text
+
+        self.assertEquals(voting_id,"501")
+        self.assertEquals(voter_id,"101")
         self.assertTrue(a_encrypted!="")
         self.assertTrue(b_encrypted!="")
+        
+        # Segundo voto correctamente guardado y encriptado
+        self.driver.find_element_by_link_text('Votes').click()
+        votes_sent= self.driver.find_elements_by_link_text('501: 101')
+        votes_sent[1].click()
+        voting_id= self.driver.find_element_by_xpath("//input[@id='id_voting_id']").get_attribute('value')
+        voter_id= self.driver.find_element_by_xpath("//input[@id='id_voter_id']").get_attribute('value')
+        a_encrypted= self.driver.find_element_by_xpath("//textarea[@id='id_a']").text
+        b_encrypted= self.driver.find_element_by_xpath("//textarea[@id='id_b']").text
 
-
+        self.assertEquals(voting_id,"501")
+        self.assertEquals(voter_id,"101")
+        self.assertTrue(a_encrypted!="" and len(a_encrypted)==77)
+        self.assertTrue(b_encrypted!="" and len(a_encrypted)==77)
 
 
 

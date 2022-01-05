83c0559247783fab6338607705a31bdb2a69c0de
jesvarzam <jvargaszambrana0@gmail.com>
2022-01-05 17:27:44 +0100

    "#79": Otra

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 import environ
 
 env = environ.Env()
-#environ.Env.read_env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
